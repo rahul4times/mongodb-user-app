b7eb5ca8a873612b1d7f0a9d7099a46f6ede7eb4
rahul4times <tkdmaster.sharma@gmail.com>
2018-01-06 20:04:06 -0700

    create user test added

--- test/create_test.js
+++ test/create_test.js
@@ -1,5 +1,25 @@
-describe('Creating records', ()=>{
-  it('Saves a user', ()=>{
+const assert = require('assert');
+const User = require('../src/user');
 
+
+// describe() & it() function takes two arguments
+// -- 1st 'string' name of test
+// -- 2nd fat arrow function
+
+// in test_helper.js done was passed in beforeEach function
+describe('Creating records', () => {
+  it('Saves a user', (done) => {
+    const joe = new User({ name: 'Joe' });
+
+    // whenever mongoose touches our database it returns a promise and
+    // that promise can be resolve or reject depends on operation
+    joe.save()
+      .then(() => {
+        // if data is in mongoose that means not inserted yet in mongodb then
+        // isNew comes as True. If it's inserted in mongodb then it's False
+        // assert is running test if data is insterted then we reach to done statement
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/user_test');
+// Here telling to mongoose that whenever you need to create promise
+// just use ES6 implementation
+mongoose.Promise = global.Promise;
 
-mongoose.connection
-  .once('open', () => console.log('Connected!'))
-  .on('error', (error) => {
-    console.log('Warning: ', error);
+// before runs only once. We want connection with database only once
+// also we want mocha to wait if database connection takes little time
+before((done) => {
+  // if there is no database then mongoose/mongodb will create it automatic
+  mongoose.connect('mongodb://localhost/user_test');
+
+  //.once and .on is event listener
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
   });
+});
+
+
+// when everytime test will run following will clear out the data
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    done();
+  });
+});
